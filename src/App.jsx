2d6b6445c8e82d508517bc563c83f47e90d66667
VincentSantana <v.santanab13@gmail.com>
2025-03-16 19:13:43 +0100

    navbar and card done

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import './App.css'
+import EventCard from './components/EventCard/EventCard'
+import Navbar from './components/Navbar/Navbar'
+import TopNavbar from './components/Navbar/TopNavbar'
 import SearchBar from './components/SearchBar'
 
 function App() {
 
   return (
     <>
-    <div>
-      <SearchBar />
-    </div>
+      <div className="flex flex-col min-h-screen">
+        <TopNavbar />
+        <SearchBar />
+        <EventCard />
+        <Navbar />
+      </div>
     </>
   )
 }
