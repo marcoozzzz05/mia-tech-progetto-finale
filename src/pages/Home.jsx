ebb35572be42879596910806d21a8f299897d8cf
ValentinaOlla <valentina_olla@tiscali.it>
2025-03-22 12:43:08 +0100

    aggiustato la visualizzazione dei post e aggiunto sezioni

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,6 @@
 import category from "../category.json"
 import EventCard from "../components/EventCard/EventCard";
-import Button2 from "../Button2";
 import Button3 from "../Button3";
-import Button1 from "../Button1";
 
 function Home() {
     console.log(category);
@@ -10,7 +8,7 @@ function Home() {
     <>
     <div className="container mx-auto px-6">
         <h2 className="text-2xl font-bold mt-10 forced-colors:[#2e2e2e]">Esplora le categorie</h2>
-    <div className="flex flex-wrap justify-center text-center mt-10 mb-20 gap-16 md:gap-20 max-w-full">
+    <div className="flex flex-wrap justify-center text-center mt-10 mb-20 gap-20 md:gap-20 max-w-full">
         {category.map((item, index) => { 
             return (
             <div key={ index } className="flex flex-col items-center">
@@ -23,11 +21,23 @@ function Home() {
     <div>
         <h2 className="text-2xl font-bold mt-10 forced-colors:[#2e2e2e]">Ultimi post aggiunti</h2>
     </div>
-    <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 justify-center gap-6 mt-10 mb-20 cursor-pointer max-w-full">
-        <EventCard />
-        <EventCard />
-        <EventCard />
-        <EventCard />
+    <div className="flex flex-wrap justify-center text-center mt-10 mb-20 gap-12 md:gap-16 cursor-pointer max-w-full">
+        <EventCard title={"Saturday Jazz: un concerto imperdibile nel cuore di Milano"} category={ "Intrattenimento" }/>
+        <EventCard title={"Sandro Cappai: Stand-Up Comedy a Cagliari"} category={ "Intrattenimento" }/>
+        <EventCard title={"Giornate FAI di Primavera"} category={ "Eventi culturali & Arte" }/>
+        <EventCard title={"Amatriciana&Carbonara Festival"} category={ "Ristorazione" }/>
+    </div>
+    <div className="flex justify-center mb-20">
+        <Button3 text={"Scopri di più"} />
+    </div>
+    <div>
+        <h2 className="text-2xl font-bold mt-10 forced-colors:[#2e2e2e]">Potrebbero interessarti</h2>
+    </div>
+    <div className="flex flex-wrap justify-center text-center mt-10 mb-20 gap-12 md:gap-16 cursor-pointer max-w-full">
+        <EventCard title={"Masterclass di Public Speaking: Comunica con Impatto"} category={ "Educazione & Formazione" }/>
+        <EventCard title={"AI e Futuro del Lavoro: Come l'Intelligenza Artificiale Sta Cambiando il Mondo"} category={ "Tecnologia & Innovazione" }/>
+        <EventCard title={"Maratona Urbana: Corri tra le Meraviglie della Città di Palermo"} category={ "Sport & Fitness" }/>
+        <EventCard title={"Hackathon 2025: Crea la Prossima Grande App!"} category={ "Tecnologia & Innovazione" }/>
     </div>
     <div className="flex justify-center mb-20">
         <Button3 text={"Scopri di più"} />
