2ddb0415294a10135c62f3ef1ba591d1b529e3e9
marcoozzzz05 <andronico.marco05@gmail.com>
2025-03-25 11:40:49 +0100

    edit register

--- src/LoginPage.jsx
+++ src/LoginPage.jsx
@@ -14,8 +14,8 @@ const LoginPage = () => {
             <div className="flex flex-col items-center gap-4 w-full max-w-lg m-30 mx-auto p-6 bg-white shadow-2xl rounded-2xl">
             <div className="font-bold text-4xl">Bentornato!</div>
             <div className="font-semibold text-xl p-4">Inserisci le tue credenziali per accedere al tuo account.</div>
-            <div className="font-semibold text-lg flex relative self-start top-2 left-2">Username</div>
-            <input type="text" placeholder="Inserisci il tuo username" 
+            <div className="font-semibold text-lg flex relative self-start top-2 left-2">Email</div>
+            <input type="text" placeholder="Inserisci la tua email" 
             className="input-with-icon w-full p-4 border border-gray-400 rounded-lg mb-4"/>
             <div className="font-semibold text-lg flex relative self-start top-2 left-2">Password</div>
             <input type={showPassword ? "text" : "password"} placeholder="Inserisci la tua password"
