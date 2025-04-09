d1672f76457df4694081e4fec140ccbb3ce13ca3
ValentinaOlla <valentina_olla@tiscali.it>
2025-04-09 10:53:27 +0200

    aggiornato settings

--- src/pages/Settings/Settings.jsx
+++ src/pages/Settings/Settings.jsx
@@ -1,38 +1,23 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom"; // Importa useNavigate
 import EditProfile from "./EditProfile";
-import LanguageCity from "./LanguageCity";
 import Button1 from "../../components/Buttons/Button1";
-import Terms from "./Terms";
-
-const settingsOptions = [
-  { name: "Edita Profilo", path: "/edit-profile" },
-  { name: "Lingua e Area Geografica", path: "/language" },
-];
 
 export default function Settings() {
-  const [selectedOption, setSelectedOption] = useState("/edit-profile");
-  const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
   const [profilo, setProfilo] = useState({});
   const navigate = useNavigate();
 
-  useEffect(() => {
-    const handleResize = () => setIsMobile(window.innerWidth < 768);
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
-
-  
+  // Recupera l'utente dal localStorage e mostra i dati
   useEffect(() => {
     const storedUser = localStorage.getItem("glokal_user");
     if (storedUser) {
-        const user = JSON.parse(storedUser);
-        setProfilo(user);
+      const user = JSON.parse(storedUser);
+      setProfilo(user);
     } else {
-        console.warn("Nessun utente loggato trovato in localStorage");
-        navigate("/");
+      console.warn("Nessun utente loggato trovato in localStorage");
+      navigate("/login");
     }
-}, [navigate]);
+  }, [navigate]);
 
   // Funzione per il logout
   const handleLogout = () => {
@@ -61,16 +46,9 @@ export default function Settings() {
           </div>
   
           <div>
-            {renderContent()}
-            <button
-              onClick={() => navigate("/settings")} // Torna al menu delle impostazioni
-              className="mt-4 bg-purple-500 text-white px-4 py-2 rounded-full"
-            >
-              Torna al Menu
-            </button>
+            <EditProfile />
           </div>
         </div>
-      )}
-    </div>
-  );
-}
+      </div>
+    );
+  }  
